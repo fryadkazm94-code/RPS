f46c8d0466ed110a219f1457cea4eb4aad9d44ed
fryadkazm <fryadkazm94@gmail.com>
2025-12-19 16:36:28 -0800

    update index

--- logic/index.js
+++ logic/index.js
@@ -1,3 +1,27 @@
 "use strict";
+
 document.querySelector(".copyright").textContent = new Date().getFullYear();
-// alternative text for the rock image: A closed fist representing rock in the rock paper scissors game
+
+// selecting the button that actually starts the game(changes the images)
+const play = document.querySelector(".play");
+
+// selecting the result area(the area that will display the winner and looser)
+const result = document.querySelector(".result");
+
+// selecting both images
+const firstImage = document.querySelector(".first-image");
+const secondImage = document.querySelector(".second-image");
+
+const images = [
+  "../images/rock.webp",
+  "../images/paper.webp",
+  "../images/scissor.webp",
+];
+
+play.addEventListener("click", function () {
+  const userRandomNumber = Math.trunc(Math.random() * images.length);
+  const computerRandomNumber = Math.trunc(Math.random() * images.length);
+
+  firstImage.src = images[userRandomNumber];
+  secondImage.src = images[computerRandomNumber];
+});
